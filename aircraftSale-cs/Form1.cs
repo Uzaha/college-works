84d2303856fece0c2bf8a86ece860ab100671c5e
Pedro Henrique Souza <psouza.1650@gmail.com>
2021-09-23 00:54:35 -0300

    initial commit

---
+++ aircraftSale-cs/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace aircraftSale
+{
+    public partial class FormAircraft : Form
+    {
+        public FormAircraft()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "aircraftsale";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            textBoxAircraft.Clear();
+            textBoxManufacturing.Clear();
+            textBoxTurbulence.Clear();
+            textBoxPassenger.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FormAircraft_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void atualizarGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexaoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexaoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexaoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM inventory";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dataGridInventory.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridInventory.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetString(0);
+                    row.Cells[1].Value = reader.GetString(1);
+                    row.Cells[2].Value = reader.GetString(2);
+                    row.Cells[3].Value = reader.GetString(3);
+                    row.Cells[4].Value = reader.GetString(4);
+                    dataGridInventory.Rows.Add(row);
+                }
+                realizaConexaoBD.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Can not opne connection !");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexaoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexaoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexaoBD.CreateCommand();
+                comandoMySql.CommandText = "INSERT INTO inventory (aircraft, manufacturing, wakeTurbulenceCategory, passengerCapacity)" + "VALUES('" + textBoxAircraft.Text + "', " + Convert.ToInt16(textBoxManufacturing.Text) + ", '" + textBoxTurbulence.Text + "', " + Convert.ToInt16(textBoxPassenger.Text) + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexaoBD.Close();
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
